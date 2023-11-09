cbde2aa214b67f33f139e3b2a3941146fd483abb
azizbek400 <aziznormurodov400@gmail.com>
2023-11-10 00:03:34 +0500

    first commit

---
+++ style.js
@@ -0,0 +1,34 @@
+// 1-mashq ikkita butun son berilgan. Shu sonlarning kattasini aniqlovchi programma tuzing.
+
+let a = parseInt(Math.random() *100)
+let b = parseInt(Math.random() *100)
+
+console.log (a)
+console.log (b)
+
+
+if ( a > b) {
+    console.log('a soni katta')
+} else {
+    console.log('b soni katta')
+}
+
+
+// 2-mash ikkita butun son berilgan. Shu sonlarning kichigini aniqlovchi programma tuzing.
+
+let c = parseInt(Math.random () *200)
+let d = parseInt(Math.random () *200)
+
+console.log (c)
+console.log (d)
+
+
+if ( c < d) {
+    console.log('c soni kichik')
+} else {
+    console.log('d soni kichik')
+}
+
+
+
+
